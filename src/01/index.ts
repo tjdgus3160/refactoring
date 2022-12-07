3eb139d49b65bf17c82464fc84d8d0c743da3541
tjdgus3160 <tjdgus3160@gmail.com>
2022-12-08 00:50:18 +0900

    1장: totalAmount 함수 추출

--- src/01/index.ts
+++ src/01/index.ts
@@ -3,17 +3,15 @@ import plays from "./data/plays.json";
 import { Invoice, Performance } from "../../@types";
 
 export function statement(invoice: Invoice) {
-  let totalAmount = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${usd(totalAmount)}\n`;
+  result += `총액: ${usd(totalAmount(invoice))}\n`;
   result += `적립 포인트: ${totalVolumeCredits(invoice)}점\n`;
 
   return result;
@@ -71,6 +69,14 @@ function totalVolumeCredits(invoice: Invoice) {
   let result = 0;
   for (let perf of invoice.performances) {
     result += volumeCreditsFor(perf);
+  }
+  return result;
+}
+
+function totalAmount(invoice: Invoice) {
+  let result = 0;
+  for (let perf of invoice.performances) {
+    result += amountFor(perf);
   }
   return result;
 }
