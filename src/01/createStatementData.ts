e20a0273d487155fe780bc1880b513c236e22c29
tjdgus3160 <tjdgus3160@gmail.com>
2022-12-08 02:30:13 +0900

    1장: 서브 클래스 생성

--- src/01/createStatementData.ts
+++ src/01/createStatementData.ts
@@ -29,7 +29,14 @@ function enrichPerformance(aPerformance: Performance): Performance {
 }
 
 function createPerformanceCalculator(aPerformance: Performance, aPlay: Play) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${this.play.type}`);
+  }
 }
 
 function totalAmount(data: StatementData) {
@@ -42,4 +49,25 @@ function totalVolumeCredits(data: StatementData) {
 
 function playFor(aPerformance: Performance) {
   return plays[aPerformance.playID];
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
 }
