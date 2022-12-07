4d3da6eabac7afc46313b6ac02da22c6da20d930
tjdgus3160 <tjdgus3160@gmail.com>
2022-12-08 00:52:22 +0900

    1장: 본문 전체를 renderPlainText 함수로 추출

--- src/01/index.ts
+++ src/01/index.ts
@@ -3,6 +3,10 @@ import plays from "./data/plays.json";
 import { Invoice, Performance } from "../../@types";
 
 export function statement(invoice: Invoice) {
+  return renderPlainText(invoice);
+}
+
+function renderPlainText(invoice: Invoice) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
