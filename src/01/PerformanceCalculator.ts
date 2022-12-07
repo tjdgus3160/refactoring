01ae7d4365a9d98311e1c2652a8dfdd56e248815
tjdgus3160 <tjdgus3160@gmail.com>
2022-12-08 02:42:46 +0900

    1장: volumeCredits 오버라이드

--- src/01/PerformanceCalculator.ts
+++ src/01/PerformanceCalculator.ts
@@ -3,15 +3,7 @@ import { Performance, Play } from "../../@types";
 export default class PerformanceCalculator {
   constructor(public performance: Performance, public play: Play) {}
 
-  get volumeCredits() {
-    let result = 0;
-
-    result += Math.max(this.performance.audience - 30, 0);
-
-    if (this.play.type === "comedy") {
-      result += Math.floor(this.performance.audience / 5);
-    }
-
-    return result;
+  public get volumeCredits() {
+    return Math.max(this.performance.audience - 30, 0);
   }
 }
--- src/01/createStatementData.ts
+++ src/01/createStatementData.ts
@@ -70,4 +70,8 @@ class ComedyCalculator extends PerformanceCalculator {
     result += 300 * this.performance.audience;
     return result;
   }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
+  }
 }
