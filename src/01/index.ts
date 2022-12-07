1fe03ecc102cd661152864694f7b2182f375734f
tjdgus3160 <tjdgus3160@gmail.com>
2022-12-08 01:47:47 +0900

    1장: 반복문 파이프라인으로 변경

--- src/01/index.ts
+++ src/01/index.ts
@@ -84,20 +84,12 @@ function enrichPerformance(aPerformance: Performance): Performance {
   return result;
 }
 
-function totalVolumeCredits(data: StatementData) {
-  let result = 0;
-  for (let perf of data.performances) {
-    result += perf.volumeCredits;
-  }
-  return result;
+function totalAmount(data: StatementData) {
+  return data.performances.reduce((total, p) => total + p.amount, 0);
 }
 
-function totalAmount(data: StatementData) {
-  let result = 0;
-  for (let perf of data.performances) {
-    result += perf.amount;
-  }
-  return result;
+function totalVolumeCredits(data: StatementData) {
+  return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
 }
 
 console.log(statement(invoices[0]));
