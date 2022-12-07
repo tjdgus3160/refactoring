adbbdb033bf40534ad6075710d8e8f2b9c133016
tjdgus3160 <tjdgus3160@gmail.com>
2022-12-08 01:59:10 +0900

    1장: renderHTML 및 테스트 추가

--- src/01/index.ts
+++ src/01/index.ts
@@ -2,7 +2,13 @@ import invoices from "./data/invoices.json";
 import plays from "./data/plays.json";
 import { Invoice, Performance, Play, StatementData } from "../../@types";
 
-export function statement(invoice: Invoice) {
+export const statement = (invoice: Invoice) =>
+  renderPlainText(createStatementData(invoice));
+
+export const htmlStatement = (invoice: Invoice) =>
+  renderHtml(createStatementData(invoice));
+
+function createStatementData(invoice: Invoice) {
   const statementData: StatementData = {
     customer: invoice.customer,
     performances: invoice.performances.map(enrichPerformance),
@@ -10,7 +16,24 @@ export function statement(invoice: Invoice) {
 
   statementData.totalAmount = totalAmount(statementData);
   statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return renderPlainText(statementData);
+
+  return statementData;
+}
+
+function renderHtml(data: StatementData) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</hi>\n`;
+  result += `<table>\n`;
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n";
+  for (let perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>${
+      perf.audience
+    }</td><td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${usd(data.totalVolumeCredits)}</em>점</p>\n`;
+
+  return result;
 }
 
 function renderPlainText(data: StatementData) {
@@ -93,3 +116,4 @@ function totalVolumeCredits(data: StatementData) {
 }
 
 console.log(statement(invoices[0]));
+console.log(htmlStatement(invoices[0]));
--- test/01.spec.ts
+++ test/01.spec.ts
@@ -1,12 +1,17 @@
 import invoices from "../src/01/data/invoices.json";
 import plays from "../src/01/data/plays.json";
-import { statement } from "../src/01/index";
+import { htmlStatement, statement } from "../src/01/index";
 import { Plays } from "../@types";
 
 describe("연극 비용 책정", () => {
-  test("올바른 인자가 전달되면 바르게 출력된다.", () => {
+  test("plainText로 잘 나온다.", () => {
     expect(statement(invoices[0])).toBe(
       "청구 내역 (고객명: BigCo)\n  Hamlet: $650.00 (55석)\n  As Your Like It: $580.00 (35석)\n  Othello: $500.00 (40석)\n총액: $1,730.00\n적립 포인트: 47점\n"
+    );
+  });
+  test("HTML로 잘 나온다.", () => {
+    expect(htmlStatement(invoices[0])).toBe(
+      "<h1>청구 내역 (고객명: BigCo)</hi>\n<table>\n<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n  <tr><td>As Your Like It</td><td>35</td><td>$580.00</td></tr>\n  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n</table>\n<p>총액: <em>$1,730.00</em></p>\n<p>적립 포인트: <em>$0.47</em>점</p>\n"
     );
   });
 });
